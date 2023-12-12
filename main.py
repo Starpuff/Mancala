3a49761dd43a64c9c875c11c1a5c0df6c7097f8b
Starpuff <ioana.hanus1@gmail.com>
2023-12-12 21:19:16 +0200

    adding board -- done

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
 import pygame
 
 pygame.init()
-screen_width = 1280
-screen_height = 720
-screen = pygame.display.set_mode((screen_width, screen_height))
+SCREEN_WIDTH = 1280
+SCREEN_HEIGHT = 720
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 pygame.display.set_caption("Mancala")
 
 background_image = pygame.image.load('Images/background.png').convert()
-background_image = pygame.transform.scale(background_image, (screen_width, screen_height))
+background_image = pygame.transform.scale(background_image, (SCREEN_WIDTH, SCREEN_HEIGHT))
 
 board_image = pygame.image.load('Images/mancala_board.png').convert_alpha()
 board_width = board_image.get_width() * 0.75
@@ -19,8 +19,8 @@ done = False
 while not done:
     screen.blit(background_image, (0, 0))
 
-    overlay_position = (screen_width // 2 - board_image.get_width() // 2,
-                        screen_height // 2 - board_image.get_height() // 2)
+    overlay_position = (SCREEN_WIDTH // 2 - board_image.get_width() // 2,
+                        SCREEN_HEIGHT // 2 - board_image.get_height() // 2)
     screen.blit(board_image, overlay_position)
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
